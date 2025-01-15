70903f568e375c60d6159ec5abb34bb7133bfeb3
Jake Marlow <jakemarlow@SquirtBook-Pro.local>
2025-01-14 22:21:09 -0500

    added routes

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<!-- src/routes/about/+page.svelte -->
+<script>
+	export let params;
+</script>
+
+<h1>About Me</h1>
+<p>Welcome to my About page!</p>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,7 @@
+<!-- src/routes/contact/+page.svelte -->
+<script>
+	export let params;
+</script>
+
+<h1>My Porjects</h1>
+<p>in detail</p>
