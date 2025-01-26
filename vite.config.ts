583eae96adb2283f1f1fec1067e21bda8d1ee6f1
Jake Marlow <jakemarlow@SquirtBook-Pro-8.local>
2025-01-25 22:52:19 -0500

    added about page

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: '0.0.0.0', // Bind to all IPs
-		port: 5173
-	}
+	plugins: [sveltekit()]
 });
